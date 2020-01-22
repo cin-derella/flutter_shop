55906a40ce308c2b5cc5dc632c3ca68845d62ace
cin-derella <shirley_cinderella@outlook.com>
2020-01-22 15:50:16 +0800

    use nested model and providers

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,42 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop/models/product.dart';
+import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius:BorderRadius.circular(8),
           child: GridTile(
             child:GestureDetector(
               onTap: (){
-                Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+                Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: product.id);
               },
               child: Image.network(
-          imageUrl,
+          product.imageUrl,
           fit: BoxFit.cover,
         ),),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
             color: Theme.of(context).accentColor,
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavoritesStatus();
+            },
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
             color: Theme.of(context).accentColor,
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () {
+              ;
+            },
           ),
         ),
       ),

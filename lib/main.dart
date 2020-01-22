4348c33eaa5595adb57b0ce2617eca529ed5f5d1
cin-derella <shirley_cinderella@outlook.com>
2020-01-22 11:49:44 +0800

    add navigator to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,9 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.deepOrange,
       ),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName:(ctx)=>ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/models/product.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -13,10 +14,14 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius:BorderRadius.circular(8),
           child: GridTile(
-        child: Image.network(
+            child:GestureDetector(
+              onTap: (){
+                Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+              },
+              child: Image.network(
           imageUrl,
           fit: BoxFit.cover,
-        ),
+        ),),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(

52c53e6f3110fce7b3121d2863e8f0b6bcfb26b4
cin-derella <shirley_cinderella@outlook.com>
2020-01-23 12:16:31 +0800

    add snackbar

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -23,13 +23,13 @@ class Cart with ChangeNotifier {
   }
 
   int get itemCount {
-    return  _items.length;
+    return _items.length;
   }
 
-  double get totalAmount{
+  double get totalAmount {
     var total = 0.0;
-    _items.forEach((key,cartItem){
-        total += cartItem.price * cartItem.quantity;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
     });
     return total;
   }
@@ -56,15 +56,35 @@ class Cart with ChangeNotifier {
         ),
       );
     }
-      notifyListeners();
+    notifyListeners();
   }
 
-  void removeItem(String productId){
+  void removeItem(String productId) {
     _items.remove(productId);
     notifyListeners();
   }
 
-  void clear(){
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity-1,
+        ),
+      );
+    }else{
+      _items.remove(productId);    
+      }
+      notifyListeners();
+  }
+
+  void clear() {
     _items = {};
     notifyListeners();
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -14,7 +14,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-    final cart = Provider.of<Cart>(context,listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     print('product rebuild');
     return ClipRRect(
       borderRadius: BorderRadius.circular(8),
@@ -36,7 +36,6 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor,
               icon: Icon(
                   product.isFavorite ? Icons.favorite : Icons.favorite_border),
- 
               onPressed: () {
                 product.toggleFavoritesStatus();
               },
@@ -51,9 +50,18 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
-             
               cart.addItem(product.id, product.price, product.title);
-               print('data passed');
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Item added to cart!'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(label: 'UNDO',onPressed: (){
+                    cart.removeSingleItem(product.id);
+
+                  },),
+                ),
+              );
             },
           ),
         ),
